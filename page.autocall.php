15f13128bc856afb022251f82a03ae0b35580d35
Waitancheg <aleksey.gira@gmail.com>
2016-09-19 15:43:31 +0300

    last

--- page.autocall.php
+++ page.autocall.php
@@ -5,6 +5,41 @@
  * Date: 9/19/16
  * Time: 2:43 PM
  */
+//$uploaddir = '/var/www/html/admin/modules/autocall/conf/';
+//$findstr = "/usr/bin/php /var/www/html/admin/modules/autocall/robot.php";
+//$files = glob($uploaddir . "*.php");
+//$crons = autocall_list_cron('list',$findstr);
+if(isset($_GET["upload"])) {
+
+//    echo json_encode( $data );
+}else{
+    start_page();
+}
+function start_page()
+{
+    ?>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-9">
+                <div class="fpbx-container">
+                    <div id ="mainframe">
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3 hidden-xs " >
+                <div class="list-group" >
+                    <a href=# id="choose_firms" class="list-group-item" data-controls="firms">Настройка фирм</a >
+                    <a href=# id="schedule" class="list-group-item" data-controls="schedule"> Расписания</a >
+                    <a href=# id="queue" class="list-group-item" data-controls="queue"> Очередь обзвона </a >
+                    <!--                    <a href=# id="called_list" class="list-group-item" > Список Совершенных Звонков </a >-->
+                    <!--                    <a href=# id="test" class="list-group-item" > TEST </a >-->
+                </div >
+            </div >
+        </div>
+    </div>
+    <div id="formodal"></div>
+
+    <?php
+}
 ?>
-Hello World
-Hello World1
